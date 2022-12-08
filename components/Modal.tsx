5817c25df78f108086337e55eae384aefc645487
Sarah-Angel kiboma <sarah-angelkiboma@IDX-161639INVL.local>
2022-12-08 16:29:26 +0300

    modified details modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,6 +1,6 @@
 import MuiModal from "@mui/material/Modal"
 import { useRecoilState } from "recoil"
-import { XIcon, PlusIcon } from "@heroicons/react/outline"
+import { XIcon, PlusIcon, ThumbUpIcon, VolumeOffIcon, VolumeUpIcon } from "@heroicons/react/outline"
 import ReactPlayer from 'react-player/lazy'
 import { FaPlay } from 'react-icons/fa'
 import { modalState, movieState } from "../atoms/modalAtom"
@@ -59,6 +59,7 @@ function Modal() {
         </button>
 
         <div className="relative pt-[56.25%]">
+          {/* TODO: add fallback video */}
           <ReactPlayer
             url={`https://www.youtube.com/watch?v=${trailer}`}
             width="100%"
@@ -73,10 +74,58 @@ function Modal() {
                 <FaPlay className="h-7 w-7 text-black" />
                 Play
               </button>
-              <button className="flex items-center gap-x-2 rounded bg-white">
-                <PlusIcon className="h-7 w-7 text-black" />
-                Play
+              <button className="modalButton">
+                <PlusIcon className="h-7 w-7" />
               </button>
+              <button className="modalButton">
+                <ThumbUpIcon className="h-7 w-7" />
+              </button>
+            </div>
+
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ?
+                <VolumeOffIcon className="h-6 w-6" />
+                :
+                <VolumeUpIcon className="h-6 w-6" />
+              }
+            </button>
+          </div>
+        </div>
+
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs" >
+                HD
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">
+                {movie?.overview}
+              </p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres: </span>
+                  {genres.map((genre) => genre.name).join(', ')}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Original language: </span>
+                  {movie?.original_language}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Total votes: </span>
+                  {movie?.vote_count}
+                </div>
+              </div>
             </div>
           </div>
         </div>
